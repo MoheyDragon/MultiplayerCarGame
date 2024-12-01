19695980708ef07dbbcc479e9e554aacaa5379a0
mohey_aldeen alMardiny <mohey.al.deen94@gmail.com>
2024-12-01 22:02:01 +0400

    Spawn different players

--- Assets/Scripts/Networking/PlayersSpwaner.cs
+++ Assets/Scripts/Networking/PlayersSpwaner.cs
@@ -3,10 +3,21 @@ using UnityEngine;
 
 public class PlayersSpwaner : SimulationBehaviour,IPlayerJoined
 {
-    [SerializeField] GameObject playerPrefab;
+    [SerializeField] GameObject[] playerCars;
+    [SerializeField] Transform spawnPositionsParent;
+    Vector3[] spawnPositions;
+    private void Awake()
+    {
+        spawnPositions=new Vector3[spawnPositionsParent.childCount];
+        for(int i = 0; i < spawnPositionsParent.childCount; i++)
+        {
+            spawnPositions[i]=spawnPositionsParent.GetChild(i).position;
+        }
+    }
     public void PlayerJoined(PlayerRef player)
     {
+        int playerNumber = Runner.SessionInfo.PlayerCount - 1;
         if (player == Runner.LocalPlayer)
-            Runner.Spawn(playerPrefab, Vector3.zero);
+            Runner.Spawn(playerCars[playerNumber], spawnPositions[playerNumber]);
     }
 }
